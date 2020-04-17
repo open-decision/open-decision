43396e191743941390740b85e9376cf90bd52aaa
Philipp Garbowsky <philipp@pop-os.localdomain>
2020-04-17 16:20:32 +0200

    integrated theme-ui

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 //react-hot-loader needs to be imported before react and react-dom
+/** @jsx jsx */
 import { hot } from "react-hot-loader/root";
-import React from "react";
+// import React from "react";
 import styled from "@emotion/styled";
+import { jsx, ThemeProvider } from "theme-ui";
+import theme from "./theme";
 
 const StyledApp = styled.div`
   margin: 1rem;
-  font-family: Arial, Helvetica, sans-serif;
-  color: red;
+  color: ${(props) => props.theme.colors.primary};
 `;
 
 function App() {
   return (
-    <StyledApp>
-      <h1>Test</h1>
-    </StyledApp>
+    <ThemeProvider theme={theme}>
+      <StyledApp>
+        <h1>Test</h1>
+      </StyledApp>
+    </ThemeProvider>
   );
 }
 
