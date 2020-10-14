cb5050821c836b573c068b5718a2e7fadc70e5ff
PhilGarb <phil.garbowsky@philtech.dev>
2020-10-14 18:19:06 +0200

    changed Provider to urql

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "core-js/stable";
 import "regenerator-runtime/runtime";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "theme-ui";
 import { App } from "./App";
 import theme from "./theme";
-import { ApolloProvider } from "@apollo/client";
-import { client } from "./ApolloClient";
+// import { ApolloProvider } from "@apollo/client";
+import { client } from "./Features/Data/DataFetchingClient";
+import { Provider } from "urql";
 
 const AppContext = () => {
   return (
     <BrowserRouter>
-      <ApolloProvider client={client}>
+      <Provider value={client}>
         <ThemeProvider theme={theme}>
           <App />
         </ThemeProvider>
-      </ApolloProvider>
+      </Provider>
     </BrowserRouter>
   );
 };
