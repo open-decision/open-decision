3371cf5d31cd6a695dd5395ae49b3295af3cbf62
PhilGarb <phil.garbowsky@philtech.dev>
2020-10-17 15:13:34 +0200

    removed backend integration

--- src/backend-integration/dataAccessors/getAllTreeData.ts
+++
@@ -1,9 +0,0 @@
-// import { pathOr, pipe, map } from "remeda";
-// import { allTreeData, treeObject } from "types/global";
-
-// export const getAllTreeData: allTreeData = (data) => {
-//   const parseTags = (data: treeObject) =>
-//     pipe(data, pathOr(["node", "tags"], ""), JSON.parse);
-
-//   return pipe(data, pathOr(["allDecisionTrees", "edges"], []), map(parseTags));
-// };
--- src/backend-integration/index.ts
+++
@@ -1,9 +0,0 @@
-export * from "./fetchDatabase";
-
-//queries
-export * from "./queries/allTrees";
-export * from "./queries/singleTree";
-
-//dataAccessors
-export * from "./dataAccessors/getAllTreeData";
-export * from "./dataAccessors/getSingleTreeData";
