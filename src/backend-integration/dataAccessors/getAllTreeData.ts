62bdef86ed60db5b1fac47278687734a39b694de
PhilGarb <phil.garbowsky@philtech.dev>
2020-10-17 15:07:49 +0200

    commented out function

--- src/backend-integration/dataAccessors/getAllTreeData.ts
+++ src/backend-integration/dataAccessors/getAllTreeData.ts
@@ -1,25 +1,9 @@
-import { pathOr, pipe, map } from "remeda";
+// import { pathOr, pipe, map } from "remeda";
+// import { allTreeData, treeObject } from "types/global";
 
-interface treeObject {
-  node: {
-    id: string;
-    name: string;
-    slug: string;
-    tags?: [];
-    createdAt: string;
-  };
-}
+// export const getAllTreeData: allTreeData = (data) => {
+//   const parseTags = (data: treeObject) =>
+//     pipe(data, pathOr(["node", "tags"], ""), JSON.parse);
 
-interface allTreeData {
-  (data: { allDecisionTrees: { edges: treeObject[] } }): Record<
-    string,
-    treeObject
-  >[];
-}
-
-export const getAllTreeData: allTreeData = (data) => {
-  const parseTags = (data: treeObject) =>
-    pipe(data, pathOr(["node", "tags"], ""), JSON.parse);
-
-  return pipe(data, pathOr(["allDecisionTrees", "edges"], []), map(parseTags));
-};
+//   return pipe(data, pathOr(["allDecisionTrees", "edges"], []), map(parseTags));
+// };
