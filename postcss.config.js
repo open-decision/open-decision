0d9448065e9a7365232dfb3a96a1a7e194ffdc96
PhilGarb <phil.garbowsky@philtech.dev>
2020-10-29 12:46:34 +0100

    added purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,12 @@
+// @ts-nocheck
 module.exports = {
-  plugins: [require("tailwindcss"), require("autoprefixer")],
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    process.env.NODE_ENV === "production" &&
+      require("@fullhuman/postcss-purgecss")({
+        content: ["./src/**/*.tsx", "./src/**/*.jsx", "./public/index.html"],
+        defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+      }),
+  ],
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
   },
   purge: [],
   theme: {
