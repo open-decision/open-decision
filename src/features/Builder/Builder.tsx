f3a161730c5f50f6e94c4fc4b10f974b673c79a0
PhilGarb <phil.garbowsky@philtech.dev>
2020-11-06 18:16:14 +0100

    removed unnecessary elements from Builder component

--- src/features/Builder/Builder.tsx
+++ src/features/Builder/Builder.tsx
@@ -1,29 +1,17 @@
-import React, { FunctionComponent } from "react";
-import { Flex } from "theme-ui";
+import React from "react";
 import { NodeEditor } from "flume-test";
 import config from "./config";
 
-export const Builder: FunctionComponent<Record<string, unknown>> = () => {
+export const Builder: React.FC = () => {
   const [nodes, setNodes] = React.useState();
 
-  //TODO when Tree Data is accessible in the graphql API this function needs to be rewriten with the useQuery Hook from apollo
-
   return (
-    <Flex sx={{ alignItems: "start", flexWrap: "wrap" }}>
-      <NodeEditor
-        portTypes={config.portTypes}
-        nodeTypes={config.nodeTypes}
-        nodes={nodes}
-        onChange={setNodes}
-        defaultNodes={[
-          {
-            type: "homepage",
-            x: 190,
-            y: -150,
-          },
-        ]}
-        debug={true}
-      />
-    </Flex>
+    <NodeEditor
+      portTypes={config.portTypes}
+      nodeTypes={config.nodeTypes}
+      nodes={nodes}
+      onChange={setNodes}
+      defaultNodes={[{ type: "homepage", x: 190, y: -150 }]}
+    />
   );
 };
