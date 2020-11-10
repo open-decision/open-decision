6e34723ec7f266bf46dcfa8d4f14767939eb19e7
PhilGarb <phil.garbowsky@philtech.dev>
2020-11-10 14:59:22 +0100

    changes to Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,31 +1,27 @@
-import { Component } from "@internalTypes/global";
+import { GlobalProps } from "@internalTypes/global";
 import React from "react";
 import clsx from "clsx";
 import { pluck } from "@utils/index";
 
 const buttonVariants = {
-  base: "clickable shadow font-bold",
   primary: "bg-primary-500 hover:bg-primary-400 text-primary-900",
   secondary: "bg-secondary-500 hover:bg-secondary-400 text-secondary-900",
-  tertiary: "bg-red-500 hover:bg-red-400 text-red-100",
-  large: "text-xl py-3 px-5  md:text-2xl md:py-6 md:px-8",
+  tertiary: "bg-tertiary-500 hover:bg-tertiary-400 text-tertiary-900",
+
   normal: "py-2 px-4",
+  large: "text-xl py-3 px-5 md:text-2xl md:py-6 md:px-8",
 };
 
 type buttonVariants = typeof buttonVariants;
-type buttonLevels = keyof Pick<
+type buttonTypes = keyof Pick<
   buttonVariants,
   "primary" | "secondary" | "tertiary"
 >;
-
 type buttonSizes = keyof Pick<buttonVariants, "large" | "normal">;
 
-type Button = Component<
-  React.ButtonHTMLAttributes<HTMLButtonElement> & {
-    level?: buttonLevels;
-    size?: buttonSizes;
-    rounded?: boolean;
-  }
+type Button = React.FC<
+  React.ButtonHTMLAttributes<HTMLButtonElement> &
+    GlobalProps & { level?: buttonTypes; size?: buttonSizes; rounded?: boolean }
 >;
 
 export const Button: Button = ({
@@ -38,8 +34,9 @@ export const Button: Button = ({
 }) => (
   <button
     className={clsx(
-      pluck(buttonVariants, [level, size, "base"]),
+      pluck(buttonVariants, [level, size]),
       { rounded: rounded },
+      "shadow font-bold",
       className
     )}
     {...props}
