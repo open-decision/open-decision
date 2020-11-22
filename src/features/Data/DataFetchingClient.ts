cc8081501d48ca539c1c40637d2bd25a469148bd
PhilGarb <phil.garbowsky@philtech.dev>
2020-11-22 16:32:00 +0100

    removed token from global requests

--- src/features/Data/DataFetchingClient.ts
+++ src/features/Data/DataFetchingClient.ts
@@ -1,13 +1,10 @@
 import { createClient, defaultExchanges } from "urql";
 import { devtoolsExchange } from "@urql/devtools";
 
-const getToken = () => localStorage.getItem("authToken");
-
 export const client = createClient({
   url: "https://builder.open-decision.org/graphql",
   exchanges: [devtoolsExchange, ...defaultExchanges],
-  fetchOptions: () => {
-    const token = getToken();
-    return token ? { headers: { authorization: `JWT ${token}` } } : {};
-  },
+  // fetchOptions: {
+  //   credentials: "include",
+  // },
 });
