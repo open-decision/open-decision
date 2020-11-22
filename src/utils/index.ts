ec49c6dd8684dc4f8474f9889214f518ee3d0bb2
PhilGarb <phil.garbowsky@philtech.dev>
2020-11-22 16:28:20 +0100

    switched arguments of pluck around

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,7 @@
 export * from "./hooks";
 export const pluck = <T, K extends keyof T>(
-  o: T,
-  propertyNames: K[]
+  propertyNames: K[],
+  o: T
 ): T[K][] => {
   return propertyNames.map((n) => o[n]);
 };
