5e7185ba9791ea6a1c21d79ed46580700384a644
PhilGarb <phil.garbowsky@philtech.dev>
2020-11-23 17:16:21 +0100

    import/export for trees

--- src/features/Builder/Builder.tsx
+++ src/features/Builder/Builder.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { NodeEditor, Nodes } from "flume";
 import config from "./config";
+import { Button } from "@components/index";
 
-export const Builder: React.FC = () => {
-  const [nodes, setNodes] = React.useState<Nodes>({});
+const Editor: React.FC = ({ initialNodes }) => {
+  const [nodes, setNodes] = React.useState<Nodes>(initialNodes);
 
   return (
     <NodeEditor
@@ -12,5 +13,34 @@ export const Builder: React.FC = () => {
       nodes={nodes}
       onChange={setNodes}
     />
+  );
+};
+
+export const Builder = () => {
+  const [nodes, setNodes] = React.useState();
+
+  return (
+    <div className="relative" style={{ backgroundColor: "#1a1c1d" }}>
+      <div className="absolute p-5 z-20 space-x-6">
+        <Button onClick={() => alert(JSON.stringify(nodes))}>Export</Button>
+        <Button
+          onClick={() => {
+            const data = prompt("Bitte Baumdaten eingeben.") || "{}";
+            const parsedData = JSON.parse(data);
+            setNodes(parsedData);
+          }}
+        >
+          Import
+        </Button>
+        <Button
+          onClick={() => {
+            setNodes();
+          }}
+        >
+          Reset
+        </Button>
+      </div>
+      {nodes ? <Editor initialNodes={nodes} /> : null}
+    </div>
   );
 };
