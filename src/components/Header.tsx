fd5e5fcd3de8dd1578571e274cbd2d104585becd
PhilGarb <phil.garbowsky@philtech.dev>
2020-11-24 14:12:22 +0100

    renamed DropdownMenu to just Dropdown, because it can take all kinds of things

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,41 +1,40 @@
 import React from "react";
 import { LogoutButton } from "@features/index";
-import { Logo, DropdownMenu } from "@components/index";
+import { Logo, Dropdown } from "@components/index";
 import { useAuth } from "@features/Auth/AuthContext";
 import { InternalLink } from "./InternalLink";
 import { UserCircleOutline } from "@graywolfai/react-heroicons";
 
 export const Header: React.FC = () => {
   const { token } = useAuth();
+  console.log(token);
 
   return (
     <div className="bg-gray-200 px-4 md:px-8 py-4">
       <div className="flex justify-between items-center">
         <Logo />
-        <div className="flex items-center space-x-8">
-          <DropdownMenu
-            icon={
-              token ? (
-                <img
-                  src="https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=256&q=80"
-                  alt="Anonymer User"
-                />
-              ) : (
-                <UserCircleOutline />
-              )
-            }
-          >
-            <InternalLink to="./profile" className="menu-link">
-              Profil
-            </InternalLink>
-            <InternalLink to="./settings" className="menu-link">
-              Einstellungen
-            </InternalLink>
-            <div className="flex space-x-6 border-t-2 pt-3 mt-6 border-gray-400">
-              <LogoutButton />
-            </div>
-          </DropdownMenu>
-        </div>
+        <Dropdown
+          icon={
+            token ? (
+              <img
+                src="https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=256&q=80"
+                alt="Anonymer User"
+              />
+            ) : (
+              <UserCircleOutline />
+            )
+          }
+        >
+          <InternalLink to="./profile" className="menu-link">
+            Profil
+          </InternalLink>
+          <InternalLink to="./settings" className="menu-link">
+            Einstellungen
+          </InternalLink>
+          <div className="flex space-x-6 border-t-2 pt-3 mt-6 border-gray-400">
+            <LogoutButton />
+          </div>
+        </Dropdown>
       </div>
     </div>
   );
