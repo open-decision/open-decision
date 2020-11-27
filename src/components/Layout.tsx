430044f09030ee8a61ffc1054eff0abd0019a2e4
PhilGarb <phil.garbowsky@philtech.dev>
2020-11-27 10:29:32 +0100

    removed Header from Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,7 @@
 import React from "react";
-import { Header } from "./Header";
-
 export const Layout: React.FunctionComponent = ({ children }) => {
   return (
-    <div
-      className="grid h-screen"
-      style={{ gridTemplateRows: "auto 1fr auto" }}
-    >
-      <Header />
+    <div className="grid h-screen" style={{ gridTemplateRows: "auto 1fr" }}>
       {children}
     </div>
   );
