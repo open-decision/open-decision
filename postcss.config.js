4161dec6f87aa560df3ed929a18bc7823d1c3b46
PhilGarb <phil.garbowsky@philtech.dev>
2020-12-05 09:05:28 +0100

    refactor: :fire: removed PurgeCSS
    purgeCSS is part of tailwind and the default configuration is fine for us

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,3 @@
-const purgecss = require("@fullhuman/postcss-purgecss");
 const tailwindcss = require("tailwindcss");
 const autoprefixer = require("autoprefixer");
 const postcssPresetEnv = require("postcss-preset-env");
@@ -13,11 +12,5 @@ module.exports = {
       },
     }),
     autoprefixer(),
-    // process.env.NODE_ENV === "production" &&
-    //   // @ts-ignore
-    //   purgecss({
-    //     content: ["./src/**/*.tsx", "./src/**/*.jsx", "./public/index.html"],
-    //     defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-    //   }),
   ],
 };
