8deea61343529cd621ade8d34467820ad4292fb6
Philipp Garbowsky <phil.garbowsky@philtech.dev>
2021-03-21 16:59:34 +0100

    feat: :sparkles: Auth token is now refreshed automatically

--- src/App.tsx
+++ src/App.tsx
@@ -5,32 +5,46 @@ import { Builder, Dashboard, LoginCard } from "features";
 import "./index.css";
 import { useRefresh_TokenMutation } from "internalTypes";
 import { useAuthStore } from "features/Data/AuthState";
+import { useTimeoutFn } from "react-use";
 
 //There are two versions of the App based around the auth state.
 //If the user is authenticated he gets the AuthenticatedApp if not he gets the UnatuhenticatedApp, which is currently just the LoginCard.
 export const App: React.FC = () => {
+  const [loading, setLoading] = React.useState(true);
+
   //When refreshing the jwt cookie is discarded. We use the refresh cookie
   //to get a new cookie so all queries are authenticated when the user is logged in.
-  const [token, login, logout, client] = useAuthStore((state) => [
-    state.token,
+  const [status, login, logout, client] = useAuthStore((state) => [
+    state.status,
     state.login,
     state.logout,
     state.client,
   ]);
 
   const auth = useRefresh_TokenMutation(client, {
-    onError: () => logout(),
-    onSuccess: ({ refreshToken }) =>
-      refreshToken ? login({ ...refreshToken }) : logout(),
+    onError: () => {
+      setLoading(false);
+      logout();
+    },
+    onSuccess: ({ refreshToken }) => {
+      setLoading(false);
+      refreshToken ? login({ ...refreshToken }) : logout();
+    },
   });
 
   //We get the mutate function specifically so we only depend on it in the Effect.
   const { mutate: getToken } = auth;
+
   React.useEffect(() => getToken({}), [getToken]);
 
-  return auth.isLoading ? (
-    <p>Loading ...</p>
-  ) : token ? (
+  console.log(loading, status);
+
+  const [isReady, cancel, reset] = useTimeoutFn(() => {
+    getToken({});
+    reset();
+  }, 30000);
+
+  return loading ? null : status === "loggedIn" ? (
     <AuthenticatedApp />
   ) : (
     <UnathenticatedApp />
