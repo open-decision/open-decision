c7da119553957a3a17a8a8cbe48c6f67b1c3fb9d
Philipp Garbowsky <phil.garbowsky@philtech.dev>
2021-04-07 14:48:14 +0200

    feat: :sparkles: Added tree query to Builder page

--- src/pages/builder/[id].tsx
+++ src/pages/builder/[id].tsx
@@ -1,11 +1,32 @@
+import { useService } from "@xstate/react";
 import { MainContent } from "components";
-import { Builder } from "features";
-import React from "react";
+import { authService, NodeEditor, Tree as TreeType } from "features";
+import { useTreeQuery } from "internalTypes";
+import { useRouter } from "next/router";
 
 export default function Tree() {
+  const [state] = useService(authService);
+  const router = useRouter();
+
+  const tree = useTreeQuery<TreeType>(
+    state.context.client,
+    { id: router.query.id as string },
+    {
+      select: ({ decisionTree }) => ({
+        config: { nodeTypes: {}, portTypes: {} },
+        state: {
+          position: { zoom: 1, coordinates: [0, 0] },
+          treeName: decisionTree?.name ?? "",
+          nodes: {},
+          edges: {},
+        },
+      }),
+    }
+  );
+
   return (
     <MainContent>
-      <Builder />
+      {tree.isSuccess && <NodeEditor tree={tree.data} />}
     </MainContent>
   );
 }
