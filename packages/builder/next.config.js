05367145e133d1d54fa1e1f0f81bafef27d8004e
PhilGarb <phil.garbowsky@philtech.dev>
2021-11-15 10:19:15 +0100

    build(builder): :wrench: Added bundle analyzer plugin

--- packages/builder/next.config.js
+++ packages/builder/next.config.js
@@ -1,8 +1,14 @@
+const withPlugins = require("next-compose-plugins");
+
 const withTM = require("next-transpile-modules")([
   "@open-legal-tech/design-system",
   "@open-decision/type-classes",
 ]);
 
-module.exports = withTM({
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withPlugins([withTM, withBundleAnalyzer], {
   target: "serverless",
 });
