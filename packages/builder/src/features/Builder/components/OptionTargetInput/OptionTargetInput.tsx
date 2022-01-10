73e3bbeba300060c1ff3d8fd3ee90fc86c67595a
PhilGarb <phil.garbowsky@philtech.dev>
2022-01-10 16:16:03 +0100

    fix(builder): :bug: Removed answer from rerendering the entire OptionTargetInput

--- packages/builder/src/features/Builder/components/OptionTargetInput/OptionTargetInput.tsx
+++ packages/builder/src/features/Builder/components/OptionTargetInput/OptionTargetInput.tsx
@@ -85,7 +85,7 @@ export function OptionTargetInputs({ node }: SingleSelectProps) {
         {relations.map((relation) => (
           <OptionTargetInput
             groupRef={ref}
-            key={`${relation.id}_${relation.answer}_${relation.target}`}
+            key={`${relation.id}_${relation.target}`}
             input={relation}
             onChange={(newData) =>
               send({
