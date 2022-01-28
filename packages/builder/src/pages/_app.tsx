6d1438129b154ec75d172ab0623b78ea04805787
PhilGarb <phil.garbowsky@philtech.dev>
2022-01-28 14:38:16 +0100

    build(builder): :construction_worker: Add Commit Hash to app to allow for better debugging

--- packages/builder/src/pages/_app.tsx
+++ packages/builder/src/pages/_app.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { Layout } from "components/Layout";
 import type { AppProps } from "next/app";
 import "../design/index.css";
-import { Box, globalStyles } from "@open-legal-tech/design-system";
+import { Box, globalStyles, Text } from "@open-legal-tech/design-system";
 import { AuthProvider, useAuth } from "features/Auth/useAuth";
 import { useRouter } from "next/router";
 import { protectedRoutes } from "../config/protectedRoutes";
@@ -14,6 +14,17 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
 
   return (
     <AuthProvider router={router}>
+      <Text
+        css={{
+          position: "absolute",
+          bottom: 20,
+          right: 20,
+          zIndex: 9999,
+          color: "$gray11",
+        }}
+      >
+        Build ID: {process.env.NEXT_PUBLIC_APP_VERSION?.substring(0, 7)}
+      </Text>
       <Layout>
         <ProtectedRoute>
           <Component {...pageProps} />
