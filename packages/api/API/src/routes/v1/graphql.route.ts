9e4c672368922546f776fd2b4970934e74ff9c72
Finn Schädlich <bennet_s@protonmail.com>
2022-02-02 00:12:51 +0100

    Add graphQL endpoint to swagger doc

--- packages/api/API/src/routes/v1/graphql.route.ts
+++ packages/api/API/src/routes/v1/graphql.route.ts
@@ -26,3 +26,25 @@ graphqlRouter.post(
 );
 
 export default graphqlRouter;
+/**
+ * @openapi
+ * tags:
+ *   name: GraphQL
+ *   description: Used to handle all data that is not part of the authentication or user management. Use  GraphQL introspection for schema documentation. Requires to be logged in.
+ */
+
+/**
+ * @openapi
+ * /graphql/:
+ *   post:
+ *     tags: [GraphQL]
+ *     summary: Root Query
+ *     description: Used to execute GraphQL queries and mutations.
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       "200":
+ *         description: OK
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ */
