3e10f8932e86e56a441469de731c3d1820bcc1f0
PhilGarb <phil.garbowsky@philtech.dev>
2022-02-24 13:19:43 +0100

    build(interpreter): :green_heart: Fixed wrong path to index file in build config

--- packages/interpreter/vite.config.js
+++ packages/interpreter/vite.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   build: {
     outDir: "lib",
     lib: {
-      entry: path.resolve(__dirname, "./index.ts"),
+      entry: path.resolve(__dirname, "./src/index.ts"),
       name: "interpreter",
       fileName: (format) => `index.${format === "es" ? "mjs" : "cjs"}`,
     },
