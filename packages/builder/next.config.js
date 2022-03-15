17031b05e25f55c528b5927d5124402e4ece3d98
PhilGarb <phil.garbowsky@philtech.dev>
2022-03-15 06:36:30 +0100

    fix(builder): :wrench: Fix yjs being imported twice breaking it

--- packages/builder/next.config.js
+++ packages/builder/next.config.js
@@ -1,25 +1,35 @@
 const withPlugins = require("next-compose-plugins");
 const { withSentryConfig } = require("@sentry/nextjs");
 
+// Yjs is transpiled, because otherwise a change in nextjs 12 results in it being imported twice. My guess is that TipTap Collaboration
+// is importing the cjs version of yjs and nextjs since version 12 imports the esm version. This results in multiple versions being imported
+// breaking the app.
+const withTM = require("next-transpile-modules")(["yjs"]);
+
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/auth/:path",
+        destination: `${process.env.OD_API_ENDPOINT}/v1/auth/:path`,
+      },
+      {
+        source: "/graphql",
+        destination: `${process.env.OD_API_ENDPOINT}/v1/graphql`,
+      },
+    ];
+  },
+};
+
 module.exports = withPlugins(
-  [withSentryConfig({}, { silent: true }), withBundleAnalyzer],
-  {
-    reactStrictMode: true,
-    async rewrites() {
-      return [
-        {
-          source: "/auth/:path",
-          destination: `${process.env.OD_API_ENDPOINT}/v1/auth/:path`,
-        },
-        {
-          source: "/graphql",
-          destination: `${process.env.OD_API_ENDPOINT}/v1/graphql`,
-        },
-      ];
-    },
-  }
+  [withTM, withSentryConfig({}, { silent: true }), withBundleAnalyzer],
+  nextConfig
 );
