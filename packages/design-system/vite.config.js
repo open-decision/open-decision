eaa35726bf0eb98d6bcba3d5423819d362dead0a
PhilGarb <phil.garbowsky@philtech.dev>
2022-03-18 15:15:44 +0100

    build: :construction_worker: Turn vite configs into esm modules so they work with packge type module

--- packages/design-system/vite.config.js
+++ packages/design-system/vite.config.js
@@ -1,20 +1,18 @@
-import path from "path";
 import { defineConfig } from "vite";
-import { peerDependencies } from "./package.json";
 import pluginReact from "@vitejs/plugin-react";
 
-module.exports = defineConfig({
+export default defineConfig({
   plugins: [pluginReact({ jsxRuntime: "classic" })],
   build: {
     outDir: "lib",
     lib: {
-      entry: path.resolve(__dirname, "./src/index.ts"),
+      entry: "./src/index.ts",
       name: "design-system",
       formats: ["es", "cjs"],
       fileName: (format) => `index.${format === "es" ? "mjs" : "cjs"}`,
     },
     rollupOptions: {
-      external: [...Object.keys(peerDependencies)],
+      external: ["@fontsource/poppins", "react", "react-dom"],
     },
   },
 });
--- packages/interpreter/vite.config.js
+++ packages/interpreter/vite.config.js
@@ -1,17 +1,15 @@
-import path from "path";
 import { defineConfig } from "vite";
-import { peerDependencies } from "./package.json";
 
-module.exports = defineConfig({
+export default defineConfig({
   build: {
     outDir: "lib",
     lib: {
-      entry: path.resolve(__dirname, "./src/index.ts"),
+      entry: "./src/index.ts",
       name: "interpreter",
       fileName: (format) => `index.${format === "es" ? "mjs" : "cjs"}`,
     },
     rollupOptions: {
-      external: [...Object.keys(peerDependencies)],
+      external: ["@open-decision/type-classes", "react"],
     },
   },
 });
