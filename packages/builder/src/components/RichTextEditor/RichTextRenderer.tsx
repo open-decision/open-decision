5e3c6b42c3a9b3b7d0795d8bab7cb5067d4ea3cd
PhilGarb <phil.garbowsky@philtech.dev>
2022-04-17 16:48:44 +0200

    refactor(builder): :label: Better types for RichTextRenderer

--- packages/builder/src/components/RichTextEditor/RichTextRenderer.tsx
+++ packages/builder/src/components/RichTextEditor/RichTextRenderer.tsx
@@ -1,8 +1,9 @@
-import { EditorContent, useEditor } from "@tiptap/react";
 import * as React from "react";
+import { Content } from "@tiptap/core";
+import { EditorContent, useEditor } from "@tiptap/react";
 import { extensions } from "./shared";
 
-type Props = { content: any };
+type Props = { content: Content };
 
 export function RichTextRenderer({ content }: Props) {
   const editor = useEditor({ extensions, content, editable: false });
