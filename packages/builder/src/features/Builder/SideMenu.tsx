ad8aa7c03be5b2ed9fa0348735ca983a746a5cf5
PhilGarb <phil.garbowsky@philtech.dev>
2022-04-19 16:10:47 +0200

    refactor(builder): :lipstick: Add shadow to SideMenu

--- packages/builder/src/features/Builder/SideMenu.tsx
+++ packages/builder/src/features/Builder/SideMenu.tsx
@@ -1,10 +1,12 @@
-import { Box, Stack, styled, StyleObject } from "@open-decision/design-system";
+import { Stack, styled, StyleObject } from "@open-decision/design-system";
 import { Separator } from "components/Separator";
 import { CreateNodeButton } from "./CreateNodeButton";
 
 const Container = styled(Stack, {
   padding: "$2",
   alignItems: "center",
+  boxShadow: "$1",
+  zIndex: "$10",
 });
 
 type Props = { css?: StyleObject };
