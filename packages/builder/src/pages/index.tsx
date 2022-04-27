92fa6736574ce4e3f000caa1ba5ca09b6f3b8210
PhilGarb <phil.garbowsky@philtech.dev>
2022-04-27 09:09:24 +0200

    refactor(builder): :lipstick: Dashboard design adjustments

--- packages/builder/src/pages/index.tsx
+++ packages/builder/src/pages/index.tsx
@@ -1,54 +1,40 @@
-import {
-  Button,
-  Heading,
-  Icon,
-  Stack,
-  styled,
-} from "@open-decision/design-system";
+import { Box, Heading, Stack, styled } from "@open-decision/design-system";
 import { ErrorBoundary } from "@sentry/nextjs";
 import { BaseHeader, MainContent } from "components";
-import { CreateTreeDialog } from "features/Dashboard/components/Dialogs/CreateTreeDialog";
 import { TreeList } from "features/Dashboard/TreeList";
 import { ErrorFallback } from "features/Error/ErrorFallback";
 import * as React from "react";
-import { RocketIcon } from "@radix-ui/react-icons";
-import { FileImport } from "features/Dashboard/TreeImport";
+import { NewProjectDropdown } from "features/Dashboard/NewProjectDropdown";
 
 const DashboardGrid = styled(MainContent, {
   display: "grid",
   height: "100vh",
   overflow: "hidden",
   gridTemplateRows: "max-content max-content 1fr",
-  gridTemplateColumns: `1fr min(1000px, 90%) 1fr`,
-  layer: "3",
+  gridTemplateColumns: `1fr minmax(800px, 1fr) 1fr`,
+  layer: "2",
 });
 
 export default function DashboardPage() {
   return (
     <ErrorBoundary fallback={<ErrorFallback />}>
       <DashboardGrid>
-        <BaseHeader css={{ gridColumn: "1 / -1" }} />
+        <BaseHeader css={{ gridColumn: "1 / -1" }}>
+          <Box css={{ flex: 1 }} />
+        </BaseHeader>
         <Stack
           css={{
             gridColumn: 2,
             flexDirection: "row",
-            marginBlock: "$10 $8",
+            marginBlock: "$9 $7",
             justifyContent: "space-between",
           }}
         >
           <Heading size="large">Meine Projekte</Heading>
           <Stack
-            css={{ flexDirection: "row", gap: "$3", alignItems: "center" }}
+            css={{ flexDirection: "row", gap: "$2", alignItems: "center" }}
           >
-            <FileImport />
-            <CreateTreeDialog>
-              <Button>
-                <Icon css={{ marginTop: "2px" }}>
-                  <RocketIcon />
-                </Icon>
-                Neues Projekt erstellen
-              </Button>
-            </CreateTreeDialog>
+            <NewProjectDropdown />
           </Stack>
         </Stack>
         <TreeList />
