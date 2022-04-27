a296139d17a91ca64a91eab7d5dffbeb80ff64d0
PhilGarb <phil.garbowsky@philtech.dev>
2022-04-27 09:12:46 +0200

    refactor(builder): :lipstick: Design update of settings page

--- packages/builder/src/pages/settings.tsx
+++ packages/builder/src/pages/settings.tsx
@@ -1,4 +1,6 @@
 import {
+  activeStyle,
+  Box,
   Field,
   Heading,
   Icon,
@@ -10,8 +12,9 @@ import {
   SubmitButton,
   useForm,
 } from "@open-decision/design-system";
-import { AvatarIcon, EyeClosedIcon, RocketIcon } from "@radix-ui/react-icons";
+import { AvatarIcon } from "@radix-ui/react-icons";
 import { BaseHeader, MainContent } from "components";
+import { Card } from "components/Card";
 import {
   useUserUpdateMutation,
   useDeleteUserMutation,
@@ -22,26 +25,30 @@ import * as React from "react";
 
 const StyledMainContent = styled(MainContent, {
   display: "grid",
-  gridTemplateColumns: "1fr 200px 3fr 1fr",
+  gridTemplateColumns: "1fr 200px minmax(600px, 1fr) 1fr",
   gridTemplateRows: "max-content max-content 1fr",
-  layer: "3",
+  layer: "2",
   height: "100vh",
-  overflow: "hidden",
-  gap: "$8",
+  columnGap: "$7",
 });
 
 const SideMenuLink = styled("a", Label, {
+  position: "relative",
   padding: "$2 $3",
   borderRadius: "$md",
   textDecoration: "none",
+  colorScheme: "primary",
+  border: "2px solid transparent",
 
-  ...intentStyle({
-    backgroundColor: "$gray3",
+  ...activeStyle({
+    backgroundColor: "$colorScheme2",
+    borderColor: "$colorScheme6",
   }),
 
-  "&[data-active='true']": {
-    backgroundColor: "$gray5",
-  },
+  ...intentStyle({
+    backgroundColor: "$colorScheme3",
+    focusColor: "$colorScheme7",
+  }),
 });
 
 SideMenuLink.defaultProps = { size: "medium" };
@@ -67,11 +74,17 @@ export default function SettingsPage() {
 
   return (
     <StyledMainContent>
-      <BaseHeader css={{ gridColumn: "1 / -1" }} />
-      <Heading size="large" as="h1" css={{ gridColumn: "2 / 4" }}>
+      <BaseHeader css={{ gridColumn: "1 / -1" }}>
+        <Box css={{ flex: 1 }} />
+      </BaseHeader>
+      <Heading
+        size="large"
+        as="h1"
+        css={{ gridColumn: "2 / 4", marginBlock: "$9 $7" }}
+      >
         Einstellungen
       </Heading>
-      <Stack css={{ gap: "$2", gridColumn: "2" }}>
+      <Stack css={{ gap: "$1", gridColumn: "2" }}>
         <Link href="#account" passHref>
           <SideMenuLink data-active={router.asPath.includes("account")}>
             <Icon css={{ fontSize: "1.5em" }}>
@@ -80,121 +93,78 @@ export default function SettingsPage() {
             Account
           </SideMenuLink>
         </Link>
-        <Link href="#projects" passHref>
-          <SideMenuLink data-active={router.asPath.includes("projects")}>
-            <Icon css={{ fontSize: "1.5em" }}>
-              <RocketIcon />
-            </Icon>
-            Projekte
-          </SideMenuLink>
-        </Link>
-        <Link href="#display" passHref>
-          <SideMenuLink data-active={router.asPath.includes("display")}>
-            <Icon css={{ fontSize: "1.5em" }}>
-              <EyeClosedIcon />
-            </Icon>
-            Display
-          </SideMenuLink>
-        </Link>
-        <Link href="#privacy" passHref>
-          <SideMenuLink data-active={router.asPath.includes("privacy")}>
-            <Icon css={{ fontSize: "1.5em" }}>
-              <EyeClosedIcon />
-            </Icon>
-            Privacy
-          </SideMenuLink>
-        </Link>
       </Stack>
-      <Stack
-        css={{
-          gridColumn: 3,
-          overflow: "auto",
-          gap: "$6",
-        }}
-      >
-        <Heading id="account">Account</Heading>
+      <Stack css={{ gridColumn: 3 }}>
         <Stack
           css={{
-            gap: "$4",
-            layer: "1",
-            padding: "$5",
-            borderRadius: "$md",
+            gap: "$3",
           }}
         >
-          <Heading as="h3" size="small">
-            Passwort ändern
-          </Heading>
-          <PasswordForm
-            onSubmit={({ newPassword }) => mutate({ password: newPassword })}
-          >
-            <Field label="Neues Passwort">
-              <Input
-                {...registerPasswordForm("newPassword")}
-                css={{ layer: "2" }}
-                type="password"
-                placeholder="********"
-              />
-            </Field>
-            <SubmitButton
-              isLoading={isLoading}
-              variant="secondary"
-              css={{ marginLeft: "auto", marginTop: "$3" }}
-            >
+          <Card>
+            <Heading as="h3" size="small">
               Passwort ändern
-            </SubmitButton>
-          </PasswordForm>
-        </Stack>
-        <Stack
-          css={{
-            gap: "$4",
-            layer: "1",
-            padding: "$5",
-            borderRadius: "$md",
-          }}
-        >
-          <Heading as="h3" size="small" css={{ marginTop: "$4" }}>
-            Mailadresse ändern
-          </Heading>
-          <EmailForm onSubmit={({ newEmail }) => mutate({ email: newEmail })}>
-            <Field label="Neue E-Mail Adresse">
-              <Input
-                {...registerEmailForm("newEmail")}
-                css={{ layer: "2" }}
-                type="email"
-                placeholder="max.mustermann@gmx.de"
-              />
-            </Field>
-            <SubmitButton
-              isLoading={isLoading}
-              variant="secondary"
-              css={{ marginLeft: "auto", marginTop: "$3" }}
+            </Heading>
+            <PasswordForm
+              onSubmit={({ newPassword }) => mutate({ password: newPassword })}
             >
-              Mailadresse ändern
-            </SubmitButton>
-          </EmailForm>
-        </Stack>
-        <Stack
-          css={{
-            gap: "$4",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            layer: "1",
-            padding: "$5",
-            borderRadius: "$md",
-            alignItems: "center",
-          }}
-        >
-          <Heading as="h3" size="small">
-            Account löschen
-          </Heading>
-          <SubmitButton
-            onClick={() => deleteUserMutation()}
-            isLoading={isLoadingUserDelete}
-            colorScheme="danger"
-            variant="secondary"
+              <Field label="Neues Passwort">
+                <Input
+                  {...registerPasswordForm("newPassword")}
+                  css={{ layer: "2" }}
+                  type="password"
+                  placeholder="********"
+                />
+              </Field>
+              <SubmitButton
+                isLoading={isLoading}
+                variant="secondary"
+                css={{ marginLeft: "auto", marginTop: "$3" }}
+              >
+                Passwort ändern
+              </SubmitButton>
+            </PasswordForm>
+          </Card>
+          <Card>
+            <Heading as="h3" size="small" css={{ marginTop: "$4" }}>
+              E-Mail ändern
+            </Heading>
+            <EmailForm onSubmit={({ newEmail }) => mutate({ email: newEmail })}>
+              <Field label="Neue E-Mail">
+                <Input
+                  {...registerEmailForm("newEmail")}
+                  css={{ layer: "2" }}
+                  type="email"
+                  placeholder="max.mustermann@gmx.de"
+                />
+              </Field>
+              <SubmitButton
+                isLoading={isLoading}
+                variant="secondary"
+                css={{ marginLeft: "auto", marginTop: "$3" }}
+              >
+                Mailadresse ändern
+              </SubmitButton>
+            </EmailForm>
+          </Card>
+          <Card
+            css={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
           >
-            Account löschen
-          </SubmitButton>
+            <Heading as="h3" size="small">
+              Account löschen
+            </Heading>
+            <SubmitButton
+              onClick={() => deleteUserMutation()}
+              isLoading={isLoadingUserDelete}
+              colorScheme="danger"
+              variant="secondary"
+            >
+              Account löschen
+            </SubmitButton>
+          </Card>
         </Stack>
       </Stack>
     </StyledMainContent>
