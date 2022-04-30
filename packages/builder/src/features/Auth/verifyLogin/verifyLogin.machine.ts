fc128da6a2f4a8e1e2674c976879eca0e6bbab32
PhilGarb <phil.garbowsky@philtech.dev>
2022-04-30 15:20:58 +0200

    feat(builder): :sparkles: Added VerifyLoginDialog
    - some actions require the user to re-authenticate, because they are very
      sensitive
      - to facilitate this process the VerifyLoginDialog is used
      - instead of directly calling the action the action is passed as a
        callback to the Dialog which calls it only after successfully
        authenticating the User

---
+++ packages/builder/src/features/Auth/verifyLogin/verifyLogin.machine.ts
@@ -0,0 +1,73 @@
+import { assign, createMachine } from "xstate";
+import { VerificationFailedError } from "../errors/AuthErrors";
+import { login } from "../utils/login";
+
+type Errors = VerificationFailedError;
+
+type Events =
+  | { type: "VERIFY_LOGIN"; password: string }
+  | { type: "SUCCESSFULL_VERIFY_LOGIN" }
+  | { type: "FAILED_VERIFY_LOGIN"; error: Errors };
+
+type Context = { Error?: Errors };
+
+export type onVerify = (isVerified: boolean) => void;
+
+export const createVerifyLoginMachine = (email: string, onVerify: onVerify) =>
+  createMachine(
+    {
+      tsTypes: {} as import("./verifyLogin.machine.typegen").Typegen0,
+      initial: "unverified",
+      schema: {
+        events: {} as Events,
+        context: {} as Context,
+      },
+      on: {
+        VERIFY_LOGIN: ".verifingLogin",
+      },
+      states: {
+        unverified: {},
+        verifingLogin: {
+          invoke: {
+            src: "verifyLogin",
+          },
+          on: {
+            SUCCESSFULL_VERIFY_LOGIN: {
+              target: "verified",
+            },
+            FAILED_VERIFY_LOGIN: {
+              target: "verification_failed",
+              actions: ["assignErrorToContext"],
+            },
+          },
+        },
+        verified: { type: "final", entry: () => onVerify(true) },
+        verification_failed: { entry: () => onVerify(false) },
+      },
+    },
+    {
+      actions: {
+        assignErrorToContext: assign((_context, event) => ({
+          Error: event.error,
+        })),
+      },
+      services: {
+        verifyLogin: (_context, event) => async (send) => {
+          if (event.type !== "VERIFY_LOGIN" || !email) return;
+
+          const { password } = event;
+
+          await login(
+            email,
+            password,
+            () => send("SUCCESSFULL_VERIFY_LOGIN"),
+            (error) =>
+              send({
+                type: "FAILED_VERIFY_LOGIN",
+                error: new VerificationFailedError(error),
+              })
+          );
+        },
+      },
+    }
+  );
--- packages/type-classes/src/Error/ErrorCodes.ts
+++ packages/type-classes/src/Error/ErrorCodes.ts
@@ -1,6 +1,8 @@
 export type CommonErrors = "GENERIC_ERROR";
 
-export type ProgrammerErrors = "MISSING_CONTEXT_PROVIDER";
+export type ProgrammerErrors =
+  | "MISSING_CONTEXT_PROVIDER"
+  | "TRIED_VERIFY_UNAUTHENTICATED_USER_LOGIN";
 
 export type InterpreterErrors =
   | "GENERIC_ERROR"
@@ -9,7 +11,10 @@ export type InterpreterErrors =
   | "NO_EDGE_FOR_THRUTHY_CONDITION"
   | "NO_TRUTHY_CONDITION";
 
+export type BuilderErrors = "AUTH_VALIDATION_FAILED";
+
 export type ErrorCodes =
   | CommonErrors
   | ProgrammerErrors
-  | `INTERPRETER_${InterpreterErrors}`;
+  | `INTERPRETER_${InterpreterErrors}`
+  | `BUILDER_${BuilderErrors}`;
