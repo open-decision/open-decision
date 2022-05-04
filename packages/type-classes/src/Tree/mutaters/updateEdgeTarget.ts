2d48d88b517b81ddb8b5a7597281b22bc00ce9ac
PhilGarb <phil.garbowsky@philtech.dev>
2022-05-04 11:23:21 +0200

    fix(type-classes): :fire: Remove check for newTarget to exist when changing edge target, because it might not have been added yet

--- packages/type-classes/src/Tree/mutaters/updateEdgeTarget.ts
+++ packages/type-classes/src/Tree/mutaters/updateEdgeTarget.ts
@@ -1,11 +1,11 @@
-import { getEdge, getNode } from "../getters";
+import { getEdge } from "../getters";
 import { Tree } from "../type-classes";
 
 export const updateEdgeTarget =
   (tree: Tree.TTree) => (edgeId: string, newTarget: string) => {
     const edge = getEdge(tree)(edgeId);
-    const newTargetNode = getNode(tree)(newTarget);
-    if (!edge || !newTargetNode) return;
+
+    if (!edge) return;
 
     edge.target = newTarget;
   };
