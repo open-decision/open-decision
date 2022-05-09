93b177b73b575cc7dd31b5c6ce3605b78f68669d
PhilGarb <phil.garbowsky@philtech.dev>
2022-05-09 16:34:48 +0200

    refactor(design-system): :recycle: Update Implementation of Combobox to use ariakit

--- packages/design-system/src/Form/Combobox/index.tsx
+++
@@ -1,7 +0,0 @@
-export { Input } from "./Input";
-export type { ComboboxInputProps } from "./Input";
-export { Root } from "./Root";
-export type { ComboboxRootProps } from "./Root";
-
-export { useCombobox } from "./useCombobox";
-export type { Item } from "./useCombobox";
