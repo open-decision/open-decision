400df2ff930ebbb918276d69b8b16e2babf9cc1a
PhilGarb <phil.garbowsky@philtech.dev>
2022-05-11 17:07:16 +0200

    refactor(builder): :wrench: Change the nodeNameMaxLength validation rule to just be the length value

--- packages/builder/src/features/Builder/utilities/constants.ts
+++ packages/builder/src/features/Builder/utilities/constants.ts
@@ -1,7 +1,4 @@
 export const nodeWidth = 200;
 export const nodeHeight = 90;
 export const sidebarWidth = 540;
-export const nodeNameMaxLength = {
-  value: 70,
-  message: "Ein Knotenname kann maximal 70 Zeichen lang sein.",
-};
+export const nodeNameMaxLength = 70;
