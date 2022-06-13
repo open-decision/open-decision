042e8a482251ec1656f7037f78fcf08b8468c1cb
PhilGarb <phil.garbowsky@philtech.dev>
2022-06-13 16:59:38 +0200

    initial testing of different hosters

--- apps/renderer-app/remix.config.js
+++ apps/renderer-app/remix.config.js
@@ -2,15 +2,11 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  serverBuildTarget: "vercel",
-  // When running locally in development mode, we use the built in remix
-  // server. This does not understand the vercel lambda module format,
-  // so we default back to the standard build output.
-  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
+  serverBuildTarget: "netlify",
+  server: "./server.js",
   ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "api/index.js",
+  // serverBuildPath: ".netlify/functions-internal/server.js",
   // publicPath: "/build/",
-  serverDependenciesToBundle: [/@open-decision\/.*/],
 };
