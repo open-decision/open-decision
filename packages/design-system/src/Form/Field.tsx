c5850b4585549f2b21e2b1d1b890aba218fa850f
Philipp Garbowsky <phil.garbowsky@philtech.dev>
2022-06-17 13:11:09 +0200

    refactor(design-system): :recycle: Improve Field and Label components

--- packages/design-system/src/Form/Field.tsx
+++ packages/design-system/src/Form/Field.tsx
@@ -1,48 +1,19 @@
 import * as React from "react";
-import { styled, StyleObject } from "../stitches";
+import { StyleObject } from "../stitches";
 
-import { Box } from "../Box";
 import { Form } from ".";
 import { VisuallyHidden } from "ariakit";
-
-const FieldBox = styled(Box, {
-  display: "grid",
-  gridTemplateAreas: `"label" "input" "error"`,
-  gridAutoRows: "auto",
-  alignItems: "center",
-  columnGap: "$2",
-
-  "&[data-layout='inline-left']": {
-    gridTemplateAreas: `"label input" "error error"`,
-    gridTemplateColumns: "1fr max-content",
-  },
-
-  "&[data-layout='inline-right']": {
-    gridTemplateAreas: `"input label" "error error"`,
-    gridTemplateColumns: "max-content 1fr",
-  },
-
-  "&[data-layout='no-label']": {
-    gridTemplateAreas: `"input" "error"`,
-  },
-});
+import { Stack } from "../Layout";
 
 export type FieldProps = {
-  label: React.ReactNode;
   children: JSX.Element;
   css?: StyleObject;
   name?: string;
+  Label: React.ReactNode;
   layout?: "block" | "inline-left" | "inline-right" | "no-label";
-  customLabel?: boolean;
 };
 
-export function Field({
-  label,
-  children,
-  css,
-  layout = "block",
-  customLabel,
-}: FieldProps) {
+export function Field({ Label, children, css, layout = "block" }: FieldProps) {
   if (!React.Children.only(children)) {
     throw new Error(
       "The Field component can only ever wrap one Input as a child."
@@ -53,28 +24,50 @@ export function Field({
     throw new Error(`The Field components input child needs a name.`);
 
   const name = children.props.name;
+  const value = children.props.value;
 
   const EnhancedInput = React.cloneElement(children, {
+    id: `${name}-${value}`,
     css: { ...children.props?.css, gridArea: "input" },
   });
 
   const isLabelHidden = layout === "no-label";
 
   return (
-    <FieldBox css={css} data-layout={layout}>
-      {customLabel ? (
-        label
-      ) : isLabelHidden ? (
-        <VisuallyHidden>
-          <Form.Label name={name}>{label}</Form.Label>
-        </VisuallyHidden>
+    <Stack css={css}>
+      {isLabelHidden ? (
+        <>
+          <VisuallyHidden>
+            <Form.Label name={name}>{Label}</Form.Label>
+          </VisuallyHidden>
+          {EnhancedInput}
+        </>
       ) : (
-        <Form.Label name={name} css={{ gridArea: "label" }}>
-          {label}
+        <Form.Label
+          name={name}
+          css={{
+            display: "grid",
+            gridArea: "label",
+            gridTemplateAreas: `"label" "input"`,
+            gap: "$2",
+
+            "&[data-layout='inline-left']": {
+              gridTemplateAreas: `"label input"`,
+              gridTemplateColumns: "max-content 1fr",
+            },
+
+            "&[data-layout='inline-right']": {
+              gridTemplateAreas: `"input label"`,
+              gridTemplateColumns: "max-content max-content",
+            },
+          }}
+          data-layout={layout}
+        >
+          <span style={{ gridArea: "label" }}>{Label}</span>
+          <span style={{ gridArea: "input" }}>{EnhancedInput}</span>
         </Form.Label>
       )}
-      {EnhancedInput}
       <Form.Error name={name} css={{ gridArea: "error", marginTop: "$2" }} />
-    </FieldBox>
+    </Stack>
   );
 }
