08091d4c9cee602ed86233966aecad678c76acc5
Philipp Garbowsky <phil.garbowsky@philtech.dev>
2022-06-17 11:51:59 +0200

    feat(builder): :sparkles: Add legal checkboxes to registerForm

--- packages/design-system/src/Form/Field.tsx
+++ packages/design-system/src/Form/Field.tsx
@@ -8,13 +8,18 @@ import { VisuallyHidden } from "ariakit";
 const FieldBox = styled(Box, {
   display: "grid",
   gridTemplateAreas: `"label" "input" "error"`,
+  gridAutoRows: "auto",
+  alignItems: "center",
+  columnGap: "$2",
 
   "&[data-layout='inline-left']": {
     gridTemplateAreas: `"label input" "error error"`,
+    gridTemplateColumns: "1fr max-content",
   },
 
   "&[data-layout='inline-right']": {
     gridTemplateAreas: `"input label" "error error"`,
+    gridTemplateColumns: "max-content 1fr",
   },
 
   "&[data-layout='no-label']": {
@@ -27,10 +32,17 @@ export type FieldProps = {
   children: JSX.Element;
   css?: StyleObject;
   name?: string;
-  layout?: "block" | "inine-left" | "inline-right" | "no-label";
+  layout?: "block" | "inline-left" | "inline-right" | "no-label";
+  customLabel?: boolean;
 };
 
-export function Field({ label, children, css, layout = "block" }: FieldProps) {
+export function Field({
+  label,
+  children,
+  css,
+  layout = "block",
+  customLabel,
+}: FieldProps) {
   if (!React.Children.only(children)) {
     throw new Error(
       "The Field component can only ever wrap one Input as a child."
@@ -50,20 +62,19 @@ export function Field({ label, children, css, layout = "block" }: FieldProps) {
 
   return (
     <FieldBox css={css} data-layout={layout}>
-      {isLabelHidden ? (
+      {customLabel ? (
+        label
+      ) : isLabelHidden ? (
         <VisuallyHidden>
           <Form.Label name={name}>{label}</Form.Label>
         </VisuallyHidden>
       ) : (
-        <Form.Label name={name} css={{ gridArea: "label", marginBottom: "$2" }}>
+        <Form.Label name={name} css={{ gridArea: "label" }}>
           {label}
         </Form.Label>
       )}
       {EnhancedInput}
-      <Form.Error
-        name={name}
-        css={{ gridArea: "error", "&:not(:empty)": { marginTop: "$2" } }}
-      />
+      <Form.Error name={name} css={{ gridArea: "error", marginTop: "$2" }} />
     </FieldBox>
   );
 }
