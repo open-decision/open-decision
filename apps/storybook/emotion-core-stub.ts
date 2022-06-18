b17b72df232fb4de441eaa53f275a8d3f6a3e4c9
Philipp Garbowsky <phil.garbowsky@philtech.dev>
2022-06-18 22:10:05 +0200

    feat(storybook): :fire: Remove storybook app
    #164

--- apps/storybook/emotion-core-stub.ts
+++
@@ -1,4 +0,0 @@
-// This is necessary, because storybook depends on @emotion/core which polutes the global scope of all HTMLElements with a css prop.
-// This conflicts with the stitches css prop.
-
-export {};
