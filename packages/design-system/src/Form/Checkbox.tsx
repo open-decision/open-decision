e83518ed3320b31d05d0a4e6262eb13ebb3a55ae
Philipp Garbowsky <phil.garbowsky@philtech.dev>
2022-07-15 10:59:50 +0200

    fix focus style of checkbox

--- packages/design-system/src/Form/Checkbox.tsx
+++ packages/design-system/src/Form/Checkbox.tsx
@@ -15,7 +15,18 @@ const StyledCheckbox = styled(Box, baseInputStyles, baseInputBoxStyles, {
 export type CheckboxProps = CheckboxPrimitive.CheckboxStateProps<boolean> &
   Omit<CheckboxPrimitive.CheckboxProps, "name">;
 export const Checkbox = React.forwardRef<HTMLInputElement, CheckboxProps>(
-  function Box({ defaultValue, value, setValue, ...props }, ref) {
+  function Box(
+    {
+      defaultValue,
+      value,
+      setValue,
+      "data-test": dataTest,
+      onBlur,
+      onFocusVisible,
+      ...props
+    },
+    ref
+  ) {
     const checkbox = CheckboxPrimitive.useCheckboxState<boolean>({
       defaultValue,
       setValue,
@@ -25,20 +36,23 @@ export const Checkbox = React.forwardRef<HTMLInputElement, CheckboxProps>(
     const [focusVisible, setFocusVisible] = React.useState(false);
 
     return (
-      <label>
+      <label data-test={dataTest}>
         <VisuallyHidden>
           <CheckboxPrimitive.Checkbox
             state={checkbox}
-            onFocusVisible={() => setFocusVisible(true)}
-            onBlur={() => setFocusVisible(false)}
+            onFocusVisible={(event) => {
+              setFocusVisible(true);
+              onFocusVisible?.(event);
+            }}
+            onBlur={(event) => {
+              setFocusVisible(false);
+              onBlur?.(event);
+            }}
             ref={ref}
             {...props}
           />
         </VisuallyHidden>
-        <StyledCheckbox
-          data-checked={checkbox.value}
-          data-focus-visible={focusVisible ? "" : null}
-        >
+        <StyledCheckbox data-checked={checkbox.value} data-focus={focusVisible}>
           <CheckboxPrimitive.CheckboxCheck checked={checkbox.value} />
         </StyledCheckbox>
       </label>
