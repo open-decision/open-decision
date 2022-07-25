973b7defd33f0b8e88b9808ab4e20dbf3d110c02
Philipp Garbowsky <phil.garbowsky@philtech.dev>
2022-07-25 12:02:50 +0200

    feat(builder): Add ssr prefetching to user on settings pages

--- apps/builder/pages/settings.tsx
+++ apps/builder/pages/settings.tsx
@@ -6,19 +6,21 @@ import {
   styled,
   activeSelector,
   intentSelector,
+  LoadingSpinner,
 } from "@open-decision/design-system";
 import { AvatarIcon } from "@radix-ui/react-icons";
 import { BaseHeader } from "../components";
 import Link from "next/link";
 import * as React from "react";
 import { getDashboardLayout } from "../features/Dashboard/DashboardLayout";
-import { TGetUserOutput } from "@open-decision/user-api-specification";
 import { ChangeEmail } from "../features/Settings/ChangeEmail";
 import { ChangePassword } from "../features/Settings/ChangePassword";
 import { DeleteAccount } from "../features/Settings/DeleteAccount";
 import { GetServerSidePropsContext } from "next";
 import { client } from "@open-decision/api-client";
 import Head from "next/head";
+import { dehydrate, QueryClient } from "@tanstack/react-query";
+import { useUser } from "../features/Auth/useUserQuery";
 
 export const getServerSideProps = async (
   context: GetServerSidePropsContext
@@ -27,10 +29,10 @@ export const getServerSideProps = async (
     token: context.req.cookies["token"],
     urlPrefix: `${process.env.NEXT_PUBLIC_OD_API_ENDPOINT}/v1`,
   });
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery(["user"], () => OD.user.getUser({}));
 
-  const user = await OD.user.getUser({});
-
-  return { props: { user: user.data } };
+  return { props: { dehydratedState: dehydrate(queryClient) } };
 };
 
 const SideMenuLink = styled("a", Label, {
@@ -54,45 +56,51 @@ const SideMenuLink = styled("a", Label, {
 
 SideMenuLink.defaultProps = { size: "medium" };
 
-type PageProps = { user: TGetUserOutput };
+export default function SettingsPage() {
+  const { data: user, isLoading, isSuccess } = useUser().useUserQuery();
 
-export default function SettingsPage({ user }: PageProps) {
-  return (
-    <>
-      <Head>
-        <title>Open Decision Settings</title>
-      </Head>
-      <BaseHeader css={{ gridColumn: "1 / -1" }} />
-      <Heading
-        size="large"
-        as="h1"
-        css={{ gridColumn: "2 / 4", marginBlock: "$9 $7" }}
-      >
-        Einstellungen
-      </Heading>
-      <Stack css={{ gap: "$1", gridColumn: "2" }}>
-        <Link href="#account" passHref>
-          <SideMenuLink data-active>
-            <Icon css={{ fontSize: "1.5em" }}>
-              <AvatarIcon />
-            </Icon>
-            Account
-          </SideMenuLink>
-        </Link>
-      </Stack>
-      <Stack css={{ gridColumn: 3 }}>
-        <Stack
-          css={{
-            gap: "$3",
-          }}
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
+
+  if (isSuccess) {
+    return (
+      <>
+        <Head>
+          <title>Open Decision Settings</title>
+        </Head>
+        <BaseHeader css={{ gridColumn: "1 / -1" }} />
+        <Heading
+          size="large"
+          as="h1"
+          css={{ gridColumn: "2 / 4", marginBlock: "$9 $7" }}
         >
-          <ChangePassword user={user} />
-          <ChangeEmail user={user} />
-          <DeleteAccount user={user} />
+          Einstellungen
+        </Heading>
+        <Stack css={{ gap: "$1", gridColumn: "2" }}>
+          <Link href="#account" passHref>
+            <SideMenuLink data-active>
+              <Icon css={{ fontSize: "1.5em" }}>
+                <AvatarIcon />
+              </Icon>
+              Account
+            </SideMenuLink>
+          </Link>
         </Stack>
-      </Stack>
-    </>
-  );
+        <Stack css={{ gridColumn: 3 }}>
+          <Stack
+            css={{
+              gap: "$3",
+            }}
+          >
+            <ChangePassword user={user} />
+            <ChangeEmail user={user} />
+            <DeleteAccount user={user} />
+          </Stack>
+        </Stack>
+      </>
+    );
+  }
 }
 
 SettingsPage.getLayout = getDashboardLayout;
