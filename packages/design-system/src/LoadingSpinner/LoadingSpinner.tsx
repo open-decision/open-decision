15864ebd7d107f5e3dd7f4dee4a62d3d57fa69c9
Philipp Garbowsky <phil.garbowsky@philtech.dev>
2022-07-25 16:36:06 +0200

    feat(builder): Add isLoading option to loading spinner to show idle state

--- packages/design-system/src/LoadingSpinner/LoadingSpinner.tsx
+++ packages/design-system/src/LoadingSpinner/LoadingSpinner.tsx
@@ -3,21 +3,35 @@ import { SpinnerCircular } from "spinners-react";
 import { Icon } from "../Icon/Icon";
 import { ColorKeys } from "../internal/utils";
 import { StyleObject, theme } from "../stitches";
+import { CheckCircledIcon } from "@radix-ui/react-icons";
 
 type Props = {
   colorScheme?: ColorKeys;
   size?: React.CSSProperties["fontSize"];
   css?: StyleObject;
+  isLoading?: boolean;
 };
 
-export function LoadingSpinner({ colorScheme = "primary", size, css }: Props) {
+export function LoadingSpinner({
+  colorScheme = "primary",
+  size,
+  css,
+  isLoading = true,
+}: Props) {
   return (
-    <Icon label="Lädt" css={{ fontSize: size ?? "1.3em", ...css }}>
-      <SpinnerCircular
-        color={theme.colors[`${colorScheme}11`].value}
-        secondaryColor={theme.colors[`${colorScheme}4`].value}
-        style={{ width: "100%", height: "100%" }}
-      />
+    <Icon
+      label="Lädt"
+      css={{ fontSize: size ?? "1.3em", color: "$primary11", ...css }}
+    >
+      {isLoading ? (
+        <SpinnerCircular
+          color={theme.colors[`${colorScheme}11`].value}
+          secondaryColor={theme.colors[`${colorScheme}4`].value}
+          style={{ width: "100%", height: "100%" }}
+        />
+      ) : (
+        <CheckCircledIcon />
+      )}
     </Icon>
   );
 }
