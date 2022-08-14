4bbbcd219717b7d672bf61b76a8607c9e7130412
Philipp Garbowsky <phil.garbowsky@philtech.dev>
2022-08-14 22:18:34 +0200

    feat(api-client): Add treeStatus to tree api specification

--- packages/tree-api-specification/src/routes/trees/post/input.ts
+++ packages/tree-api-specification/src/routes/trees/post/input.ts
@@ -1,8 +1,10 @@
+import { TreeStatus } from "@open-decision/models";
 import { z } from "zod";
 
 export const createTreeInput = z.object({
   body: z.object({
     name: z.string(),
+    status: z.enum(TreeStatus).optional(),
   }),
 });
 
