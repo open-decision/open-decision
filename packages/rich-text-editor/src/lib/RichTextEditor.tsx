e6e4ab76d7a19cac5029ef393c10490645ba192c
Philipp Garbowsky <phil.garbowsky@philtech.dev>
2022-08-14 22:14:03 +0200

    feat(rich-text-editor): Make label of Rich Text Editor focus the editor

--- packages/rich-text-editor/src/lib/RichTextEditor.tsx
+++ packages/rich-text-editor/src/lib/RichTextEditor.tsx
@@ -1,3 +1,4 @@
+import * as React from "react";
 import { Box, ScrollArea, styled } from "@open-decision/design-system";
 import { EditorContent, Content, useEditor, EditorEvents } from "@tiptap/react";
 import { Toolbar } from "./Toolbar";
@@ -17,62 +18,78 @@ const StyledEditorContent = styled(EditorContent, editorStyles, {
 type Props = {
   content?: Content;
   onUpdate: (props: EditorEvents["update"]) => void;
-};
+  Label?: React.ReactNode;
+} & Omit<
+  React.ComponentProps<typeof StyledEditorContent>,
+  "editor" | "content"
+>;
 
-export const RichTextEditor = ({ content, onUpdate }: Props) => {
+export const RichTextEditor = ({
+  content,
+  onUpdate,
+  Label,
+  ...props
+}: Props) => {
   const editor = useEditor({
     extensions,
     content,
     onUpdate,
   });
 
-  return (
-    <Box
-      css={{
-        display: "grid",
-        gridTemplateRows: "50px max-content",
-        groupColor: "$colorScheme-text",
-      }}
-    >
-      <Toolbar
-        editor={editor}
-        css={{
-          border: "1px solid $gray7",
-          borderBottom: "0",
-          borderTopLeftRadius: "$md",
-          borderTopRightRadius: "$md",
-          layer: "3",
-        }}
-      />
-      <ScrollArea.Root
-        css={{
-          minHeight: "200px",
-          maxHeight: "500px",
-          layer: "2",
-          border: "1px solid $gray7",
-          borderBottomLeftRadius: "$md",
-          borderBottomRightRadius: "$md",
-          focusType: "inner-within",
-          overflow: "hidden",
-        }}
-        data-focus={editor?.isFocused}
-      >
-        <ScrollArea.Viewport
-          // Without this the RichTextRenderer cannot take up 100% of the height and would therefore not be
-          // focusable by clicking somewhere else, but the extisting text.
-          css={{
-            height: "100%",
+  const EnhancedLabel = React.isValidElement(Label)
+    ? React.cloneElement(Label, { onClick: () => editor?.commands.focus() })
+    : Label;
 
-            "& > div": {
-              height: "100%",
-              display: "block !important",
-            },
+  return (
+    <>
+      {EnhancedLabel}
+      <Box
+        css={{
+          display: "grid",
+          gridTemplateRows: "50px max-content",
+          groupColor: "$colorScheme-text",
+        }}
+      >
+        <Toolbar
+          editor={editor}
+          css={{
+            border: "1px solid $gray7",
+            borderBottom: "0",
+            borderTopLeftRadius: "$md",
+            borderTopRightRadius: "$md",
+            layer: "3",
           }}
+        />
+        <ScrollArea.Root
+          css={{
+            minHeight: "200px",
+            maxHeight: "500px",
+            layer: "2",
+            border: "1px solid $gray7",
+            borderBottomLeftRadius: "$md",
+            borderBottomRightRadius: "$md",
+            focusType: "inner-within",
+            overflow: "hidden",
+          }}
+          data-focus={editor?.isFocused}
         >
-          <StyledEditorContent editor={editor} />
-        </ScrollArea.Viewport>
-        <ScrollArea.Scrollbar />
-      </ScrollArea.Root>
-    </Box>
+          <ScrollArea.Viewport
+            // Without this the RichTextRenderer cannot take up 100% of the height and would therefore not be
+            // focusable by clicking somewhere else, but the extisting text.
+            css={{
+              height: "100%",
+
+              "& > div": {
+                height: "100%",
+                display: "block !important",
+              },
+            }}
+          >
+            <StyledEditorContent editor={editor} {...props} />
+          </ScrollArea.Viewport>
+          <ScrollArea.Scrollbar />
+        </ScrollArea.Root>
+      </Box>
+    </>
   );
 };
