73b3eb3a20524d1c2b44a1532696ba5b3acc7c8c
Philipp Garbowsky <phil.garbowsky@philtech.dev>
2022-09-04 17:48:15 +0200

    refactor: remove separate renderer app

--- apps/renderer-app/app/utils/odClient.ts
+++
@@ -1,7 +0,0 @@
-import { client } from "@open-decision/api-client";
-import { safeFetch } from "@open-decision/api-helpers";
-
-export const OD = client({
-  urlPrefix: `${process.env.NEXT_PUBLIC_OD_API_ENDPOINT}/v1`,
-  fetchFunction: safeFetch,
-});
