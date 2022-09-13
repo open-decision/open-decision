9e1619dfe9e76a28efe9df17896cb36d9ac482d4
Philipp Garbowsky <phil.garbowsky@philtech.dev>
2022-09-13 09:49:29 +0200

    refactor(api): Log everything from http and below to also capture the http request logs in production

--- apps/api/src/config/logger.ts
+++ apps/api/src/config/logger.ts
@@ -10,8 +10,8 @@ const enumerateErrorFormat = winston.format((info) => {
 
 export const logger = winston.createLogger({
   // This means that in development all logs will be printed to the console, while in other environments
-  // only logs from info and below are logged.
-  level: config.NODE_ENV === "development" ? "debug" : "info",
+  // only logs from http and below are logged.
+  level: config.NODE_ENV === "development" ? "debug" : "http",
   format: winston.format.combine(
     enumerateErrorFormat(),
     config.NODE_ENV === "development"
@@ -22,7 +22,7 @@ export const logger = winston.createLogger({
   ),
   transports: [
     new winston.transports.Console({
-      stderrLevels: ["error", "debug", "info"],
+      stderrLevels: ["error", "debug"],
     }),
   ],
 });
