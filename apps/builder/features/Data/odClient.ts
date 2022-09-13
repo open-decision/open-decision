3b2b17ca3d460f49af166bdb426b6dab75f63349
Philipp Garbowsky <phil.garbowsky@philtech.dev>
2022-09-13 09:45:08 +0200

    feat(api-client): Add ability to pass headers through context to the api client

--- apps/builder/features/Data/odClient.ts
+++ apps/builder/features/Data/odClient.ts
@@ -4,10 +4,16 @@ import { safeFetch } from "@open-decision/api-helpers";
 export const OD = client({
   urlPrefix: `${process.env["NEXT_PUBLIC_OD_API_ENDPOINT"]}/v1`,
   fetchFunction: safeFetch,
+  headers: {
+    credentials: "include",
+  },
 });
 
 export const proxiedOD = client({
   requestOrigin: "client",
   urlPrefix: `/api/external-api`,
   fetchFunction: safeFetch,
+  headers: {
+    credentials: "include",
+  },
 });
