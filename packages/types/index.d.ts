f326c5edf4525443db90786c5ed38ec606e551b9
Philipp Garbowsky <phil.garbowsky@philtech.dev>
2022-12-20 17:17:59 +0100

    refactor: Update repo configurations

---
+++ packages/types/index.d.ts
@@ -0,0 +1,7 @@
+type Messages = typeof import("@open-decision/translations").de;
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+declare interface IntlMessages extends Messages {}
+
+declare module "*.module.css";
+declare module "*.module.scss";
