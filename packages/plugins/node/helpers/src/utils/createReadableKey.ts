9a0db31ac42c19208bf0cb3772ee8354f1170d5e
Finn Schädlich <bennet_s@protonmail.com>
2023-01-04 12:16:10 +0100

    Replace umlauts with equivalent, remove all non-alphanumeric chars except "_"

--- packages/plugins/node/helpers/src/utils/createReadableKey.ts
+++ packages/plugins/node/helpers/src/utils/createReadableKey.ts
@@ -1,1 +1,11 @@
-export const createReadableKey = (key: string) => key.split(" ").join("_");
+export const createReadableKey = (key: string) =>
+  key
+    .replace(" ", "_")
+    .replace(/\u00df/g, "ss")
+    .replace(/\u00e4/g, "ae")
+    .replace(/\u00f6/g, "oe")
+    .replace(/\u00fc/g, "ue")
+    .replace(/\u00c4/g, "Ae")
+    .replace(/\u00d6/g, "Oe")
+    .replace(/\u00dc/g, "Ue")
+    .replace(/\W/g, "");
