c26a8dd46873a2628392f71e4fc2c3e1244b9e2a
Philipp Garbowsky <phil.garbowsky@philtech.dev>
2023-01-04 14:23:38 +0100

    fix(builder): Fixes paths not being creatable

--- packages/design-system/src/Form/Combobox/Combobox.tsx
+++ packages/design-system/src/Form/Combobox/Combobox.tsx
@@ -39,7 +39,7 @@ export const Item = React.forwardRef<HTMLDivElement, ItemProps>(
   ({ className, ...props }, ref) => {
     return (
       <ComboboxPrimitive.ComboboxItem
-        className={twMerge(menuItemClasses, className)}
+        className={twMerge(menuItemClasses, ["justify-between", className])}
         ref={ref}
         {...props}
       />
